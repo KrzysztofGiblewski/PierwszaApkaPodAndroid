2d39799aab2ccfc401141d989ebf5b4935e62b50
Krzysztof Giblewski <krzysztofgiblewski@gmail.com>
2019-04-27 23:30:17 +0200

    nowy setOnCliklisner

--- MainActivity.java
+++ MainActivity.java
@@ -2,15 +2,33 @@ package com.example.oktan.pierwszawandroidzie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button1;
+    private TextView textViewHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupViews();
+        textViewHello.setText("asdfghjklgggggggggg;");
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
-    Button button1=(Button)findViewById(R.id.button1);
+    public void setupViews(){
+        button1=(Button)findViewById(R.id.button1);
+        textViewHello=(TextView) findViewById(R.id.textViewHello);
+
+    }
 
 }
