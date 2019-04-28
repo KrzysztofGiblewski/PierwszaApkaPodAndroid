8b6e6a436f7628f7ffa2674589ad43eb64f16f9f
Krzysztof Giblewski <krzysztofgiblewski@gmail.com>
2019-04-28 15:42:36 +0200

    dodane jeszcze dwa textViev

--- MainActivity.java
+++ MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewHello;
     private EditText editTextPierwsza;
     private EditText editTextDruga;
+    private TextView przepisanaPierwsza;
+    private TextView przepisanaDruga;
 
 
     @Override
@@ -24,22 +26,32 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Dodawanie dodawanie=new Dodawanie();
-                int editTextPierwsza = R.id.editTextPierwsza;
-                int editTextDruga=R.id.editTextDruga;
+                Dodawanie dodawanie = new Dodawanie();
 
-                int wynikDodawania=dodawanie.wynik(editTextPierwsza,editTextDruga);
+                TextView editTextPierwsza = (TextView) findViewById(R.id.editTextPierwsza);
+                TextView editTextDruga = (TextView) findViewById(R.id.editTextDruga);
+                int pierwsza = 6;
+                int druga = 10;
 
-                textViewHello.setText("wynik dodawania= "+wynikDodawania);
+
+//                int wynikDodawania = dodawanie.wynik(editTextPierwsza,editTextDruga);
+                int wynikDodawania = dodawanie.wynik(5555, 5);
+
+                textViewHello.setText("wynik dodawania= " + wynikDodawania);
+                przepisanaPierwsza.setText(String.valueOf(pierwsza));
+                przepisanaDruga.setText(String.valueOf(druga));
 
             }
         });
     }
-    public void setupViews(){
-        button1=(Button)findViewById(R.id.button1);
-        textViewHello=(TextView) findViewById(R.id.textViewHello);
-        editTextPierwsza=(EditText)findViewById(R.id.editTextPierwsza);
-        editTextDruga=(EditText)findViewById(R.id.editTextDruga);
+
+    public void setupViews() {
+        button1 = (Button) findViewById(R.id.button1);
+        textViewHello = (TextView) findViewById(R.id.textViewHello);
+        editTextPierwsza = (EditText) findViewById(R.id.editTextPierwsza);
+        editTextDruga = (EditText) findViewById(R.id.editTextDruga);
+        przepisanaPierwsza = (TextView) findViewById(R.id.przepisanaPierwsza);
+        przepisanaDruga = (TextView) findViewById(R.id.przepisanaDruga);
 
 
     }
